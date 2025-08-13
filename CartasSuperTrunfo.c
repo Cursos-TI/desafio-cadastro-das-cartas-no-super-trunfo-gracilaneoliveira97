1b66ed080aecca6fb56359f2a84c2cc67e20f1a6
gracilaneoliveira97 <gracilane.oliveira97@gmail.com>
2025-08-13 04:02:16 +0000

    Novo comit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,20 +5,56 @@ int main(){
 // carta 1
 char estado1[10], codigo_carta1[4], cidade1[10];
 int populaçao1, pontos_turisticos1;
-float are1, pib1;
+float area1, pib1;
 
 // carta 2
 char estado2[10], codigo_carta2[4], cidade2[10];
 int populaçao2, pontos_turisticos2;
-float are2, pib2;
+float area2, pib2;
+
 //cadastro da 1 carta
+
 printf("carta 1!\n ");
 
-printf("Estado: \n ");
-scanf(" %s, Estado");
+printf("Digite seu Estado:  ");
+scanf("%s", &estado1);
 
-printf("Codigo: B01\n");
-printf("Cidade: Salvador");
+printf("Codigo da carta:  ");
+scanf("%s", &codigo_carta1);
+
+printf("Nome da cidade: ");
+scanf("%s" , &cidade1);
+
+printf("Populaçao: ");
+scanf("%d", &populaçao1);
+
+printf("Pontos turisticos: ");
+scanf("%d", &pontos_turisticos1);
+
+printf("Area: ");
+scanf("%f", &area1);
+
+printf("Pib: ");
+scanf("%f ",&pib1);
+//cadastro da carta 2
+
+printf("Carta 2!\n ");
+
+printf("Digite seu Estado:  ");
+scanf("%s", &estado2);
+
+printf("Codigo Da Carta:  ");
+scanf("%s", &codigo_carta2);
+
+printf("Nome da cidade:  ");
+scanf("%s", &cidade2);
+
+printf("Populaçao:  ");
+scanf("%d", &populaçao2);
+
+
+
+
 
 
 }   
